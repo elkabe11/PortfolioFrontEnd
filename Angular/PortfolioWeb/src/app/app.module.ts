ce642ea8c3c2ef108438719324a654f4e4dffde3
Micael <micaelgaldos@hotmail.com>
2023-01-15 16:10:42 -0300

    FrontEnd en desarrollo V3 Angular

---
+++ Angular/PortfolioWeb/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExperienciaLaboralComponent } from './componentes/experiencia-laboral/experiencia-laboral.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import{HttpClientModule} from '@angular/common/http';
+import { IniciarSesionComponent } from './componentes/iniciar-sesion/iniciar-sesion.component';
+import { PortfolioComponent } from './componentes/portfolio/portfolio.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EncabezadoComponent,
+    AcercaDeComponent,
+    ExperienciaLaboralComponent,
+    EducacionComponent,
+    SkillsComponent,
+    ProyectosComponent,
+    IniciarSesionComponent,
+    PortfolioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Angular/PortfolioWeb/src/app/componentes/educacion/educacion.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
+
+@Component({
+  selector: 'app-educacion',
+  templateUrl: './educacion.component.html',
+  styleUrls: ['./educacion.component.css']
+})
+export class EducacionComponent  {
+   
+}
